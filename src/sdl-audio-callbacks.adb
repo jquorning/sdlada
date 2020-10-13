89fec1b51266255e840a63b4e767c921978927af
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-10-13 02:24:00 +0200

    * Added generic callbacks so that arbitrary user data can be passed.

---
+++ src/sdl-audio-callbacks.adb
@@ -0,0 +1,41 @@
+--------------------------------------------------------------------------------------------------------------------
+--  Copyright (c) 2020, Vinzent "Jellix" Saranen
+--
+--  This software is provided 'as-is', without any express or implied
+--  warranty. In no event will the authors be held liable for any damages
+--  arising from the use of this software.
+--
+--  Permission is granted to anyone to use this software for any purpose,
+--  including commercial applications, and to alter it and redistribute it
+--  freely, subject to the following restrictions:
+--
+--     1. The origin of this software must not be misrepresented; you must not
+--     claim that you wrote the original software. If you use this software
+--     in a product, an acknowledgment in the product documentation would be
+--     appreciated but is not required.
+--
+--     2. Altered source versions must be plainly marked as such, and must not be
+--     misrepresented as being the original software.
+--
+--     3. This notice may not be removed or altered from any source
+--     distribution.
+--------------------------------------------------------------------------------------------------------------------
+
+with SDL.Audio.Buffers;
+
+package body SDL.Audio.Callbacks is
+
+   procedure C_Callback (Data   : in System.Address;
+                         Stream : in Buffer_Base;
+                         Length : in Interfaces.C.int) is
+      Ada_User_Data : User_Data with
+        Import     => True,
+        Convention => Ada;
+      for Ada_User_Data'Address use Data;
+   begin
+      Callback (Data   => Ada_User_Data,
+                Stream => Buffers.To_Buffer (Audio_Buf => Stream,
+                                             Audio_Len => Byte_Count (Length)));
+   end C_Callback;
+
+end SDL.Audio.Callbacks;
---
+++ src/sdl-audio-callbacks.ads
@@ -0,0 +1,41 @@
+--------------------------------------------------------------------------------------------------------------------
+--  Copyright (c) 2020, Vinzent "Jellix" Saranen
+--
+--  This software is provided 'as-is', without any express or implied
+--  warranty. In no event will the authors be held liable for any damages
+--  arising from the use of this software.
+--
+--  Permission is granted to anyone to use this software for any purpose,
+--  including commercial applications, and to alter it and redistribute it
+--  freely, subject to the following restrictions:
+--
+--     1. The origin of this software must not be misrepresented; you must not
+--     claim that you wrote the original software. If you use this software
+--     in a product, an acknowledgment in the product documentation would be
+--     appreciated but is not required.
+--
+--     2. Altered source versions must be plainly marked as such, and must not be
+--     misrepresented as being the original software.
+--
+--     3. This notice may not be removed or altered from any source
+--     distribution.
+--------------------------------------------------------------------------------------------------------------------
+--  SDL.Audio.Callbacks
+--------------------------------------------------------------------------------------------------------------------
+
+generic
+   type User_Data is private; --  type of data being passed in the callback
+   with procedure Callback (Data   : in out User_Data;
+                            Stream : in     Buffer_Type);
+   --  Your actual callback function being called by the wrapper declared in
+   --  the package which takes care of the conversion of User_Data.
+   --  For simplicity, audio data is handled as if it were just a bunch of
+   --  bytes. This should be sufficient for most purposes of such a callback.
+package SDL.Audio.Callbacks is
+
+   procedure C_Callback (Data   : in System.Address;
+                         Stream : in Buffer_Base;
+                         Length : in Interfaces.C.int) with
+     Convention => C;
+
+end SDL.Audio.Callbacks;
